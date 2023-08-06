f63f5108761e16b66563d1a8231cd538e6043c2e
Amit Singh <amitsingh19975@gmail.com>
2023-08-06 21:09:28 +0530

    add settings modal and refactor code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,44 @@
+<div class="h-full grid relative" style="grid-template-rows: 3rem calc(100% - 3rem);">
+    <header class="h-[3rem] flex px-4 items-baseline">
+        <a href={`${base}/`} aria-label="logo" class="flex items-center hover:text-sky-200 transition-colors">
+            <span class="font-bold text-[1.4em]">V</span>
+            <span class="mt-1">ocabulary</span>
+        </a>
+        <nav class="flex-grow flex justify-center -ml-[10rem]">
+            <ul class="flex items-center">
+                {#each navItems as { name, href } (href)}
+                    <li class="px-4 py-1 border-b-[3px] {hasNavItemSelected($page.route.id, href) ? 'text-[var(--bg-primary)] border-[var(--bg-primary)] font-extrabold' : 'border-transparent'}">
+                        <a href={`${base}${href}`}>{name}</a>
+                    </li>
+                {/each}
+            </ul>
+        </nav>
+        <button
+            class="transition-colors p-2 hover:bg-white hover:bg-opacity-10 active:bg-white active:bg-opacity-20 h-fit rounded-full mt-2"
+            aria-label="Open Setting"
+            title="Open Settings"
+            on:click={() => isSettingsModalOpen = true}
+        >
+            <Gear size={18} />
+        </button>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <NotificationContainer />
+    <!-- <WordAiTestModal currentWord="abhor" open={true} /> -->
+    <SettingsModal bind:open={isSettingsModalOpen} />
+</div>
+
 <script lang="ts">
     import '../app.css';
     import { base } from '$app/paths';
     import { page } from '$app/stores'
 	import { afterNavigate } from '$app/navigation';
     import NotificationContainer from '$lib/components/notificationContainer.svelte';
+    import WordAiTestModal from '$lib/components/wordAiTestModal.svelte';
+    import SettingsModal from '$lib/components/settingsModal.svelte';
+    import { Gear } from 'radix-icons-svelte';
 
     const navItems = [
         { name: 'Home', href: '/' },
@@ -12,6 +47,7 @@
     ];
 
     let animation: Animation | undefined = undefined;
+    let isSettingsModalOpen = false;
 
     afterNavigate(() => {
         const element = document.getElementById('alphabet-card-transition');
@@ -55,24 +91,3 @@
     }
 
 </script>
-<div class="h-full grid relative" style="grid-template-rows: 3rem calc(100% - 3rem);">
-    <header class="h-[3rem] flex px-4">
-        <a href={`${base}/`} aria-label="logo" class="flex items-center hover:text-sky-200 transition-colors">
-            <span class="font-bold text-[1.4em]">V</span>
-            <span class="mt-1">ocabulary</span>
-        </a>
-        <nav class="flex-grow flex justify-center -ml-[10rem]">
-            <ul class="flex items-center">
-                {#each navItems as { name, href } (href)}
-                    <li class="px-4 py-1 border-b-[3px] {hasNavItemSelected($page.route.id, href) ? 'text-[var(--bg-primary)] border-[var(--bg-primary)] font-extrabold' : 'border-transparent'}">
-                        <a href={`${base}${href}`}>{name}</a>
-                    </li>
-                {/each}
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <slot></slot>
-    </main>
-    <NotificationContainer />
-</div>
