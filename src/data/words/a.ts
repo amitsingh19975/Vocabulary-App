e83e46bd4ffbf45ca00b46059dfef1127fa77259
Amit Singh <amitsingh19975@gmail.com>
2023-08-09 22:22:07 +0530

    add words

--- src/data/words/a.ts
+++ src/data/words/a.ts
@@ -206,5 +206,97 @@ export default [
             }
         ],
         links: [GRE_LINK]
+    },
+    {
+        word: 'aesthetic',
+        forms: ['adj'],
+        defs: [
+            {
+                def: 'concerned with beauty or the appreciation of beauty or art',
+                examples: [
+                    'The aesthetic beauty of the painting is quite remarkable.',
+                    'The downfall of the empire was imminent since he was more concerned with aesthetic beauty than with the well-being of his people.'
+                ],
+            }
+        ],
+        otherForms: [
+            {
+                form: 'noun',
+                word: 'aesthetics'
+            },
+            {
+                form: 'adv',
+                word: 'aesthetically'
+            }
+        ],
+        links: [GRE_LINK]
+    },
+    {
+        word: 'affectation',
+        forms: ['noun'],
+        defs: [
+            {
+                def: 'unnatural or artificial behavior, usually intended to impress',
+                examples: [
+                    'The affectation of the politician was obvious to everyone, but still he was elected.',
+                    'In the end, the affectation was the reason why he was caught.'
+                ],
+            }
+        ],
+        links: [GRE_LINK]
+    },
+    {
+        word: 'aggrandize',
+        forms: ['verb'],
+        defs: [
+            {
+                def: 'to increase in power, influence, and reputation',
+                examples: [
+                    'The king aggrandized his power by taking over the neighboring kingdoms.',
+                    'The politician aggrandized his clutch on power by eliminating his opponents by harnessing the power of the media and fragility of the law.'
+                ],
+            }
+        ],
+        otherForms: [
+            {
+                form: 'noun',
+                word: 'aggrandizement'
+            }
+        ],
+        links: [GRE_LINK]
+    },
+    {
+        word: 'alacrity',
+        forms: ['noun'],
+        defs: [
+            {
+                def: 'cheerful willingness or eagerness',
+                examples: [
+                    'Everyone left town **with** alacrity to a higher ground when government issued a tsunami warning.',
+                    'As soon as the recess bell rang, the students left the classroom **with** alacrity.'
+                ],
+            }
+        ],
+        links: [GRE_LINK]
+    },
+    {
+        word: 'alleviate',
+        forms: ['verb'],
+        defs: [
+            {
+                def: 'to make more bearable, or to relieve the problem',
+                examples: [
+                    'The arthritis that has been making it difficult for him to walk was alleviated by the new medication.',
+                    'The parent\'s love and care alleviated the child\'s pain.'
+                ],
+            }
+        ],
+        otherForms: [
+            {
+                form: 'noun',
+                word: 'alleviation'
+            },
+        ],
+        links: [GRE_LINK]
     }
 ] as WordSchema[];
